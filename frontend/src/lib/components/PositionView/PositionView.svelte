d542af57f825bb5b97649a53c1f400260c8df427
Saiki Toshiki <toshiki.jcytp@gmail.com>
2025-01-21 22:11:03 +0900

    modify piece stand size smaller

--- frontend/src/lib/components/PositionView/PositionView.svelte
+++ frontend/src/lib/components/PositionView/PositionView.svelte
@@ -38,51 +38,7 @@
   $: position = new BoardPosition(sfen);
   let promoteChoice: PieceType = PieceType.VACANCY;
 
-  // $: sizeSet = {
-  //   borderThin: 5,
-  //   borderThic: 10,
-  //   piecePadding: 60,
-  //   boardMargin: 120,
-  //   subBoxMargin: 60,
-  //   cell: { w: 240, h: 260 },
-  //   counter: { w: 120, h: 120 },
-  //   board: {
-  //     w: 2220, // 240 * 9 + 5 * 8 + 10 * 2
-  //     h: 2400, // 260 * 9 + 5 * 8 + 10 * 2
-  //     x: 980 + (visibleMoveList ? 860 : 0), // 60 + 800 + 120
-  //     y: 120,
-  //   },
-  //   whiteStand: {
-  //     w: 800, // (240 + 60) * 2 + 60 * 3 + 10 * 2
-  //     h: 1360, // 260 * 4 + 60 * 5 + 10 * 2
-  //     x: 60 + (visibleMoveList ? 860 : 0),
-  //     y: 120,
-  //   },
-  //   blackStand: {
-  //     w: 800,
-  //     h: 1360,
-  //     x: 3320 + (visibleMoveList ? 980 : 0), // 60 + 800 + 120 + 2220 + 120
-  //     y: 1160, // 120 + 2400 - 1360
-  //   },
-  //   pieceBox: {
-  //     w: 800, // (240 + 60) * 2 + 60 * 3 + 10 * 2
-  //     h: 1680, // 260 * 5 + 60 * 6 + 10 * 2
-  //     x: 4180 + (visibleMoveList ? 860 : 0), // 60 + 800 + 120 + 2220 + 120 + 800 + 60
-  //     y: 840, // 120 + 2400 - 1680
-  //   },
-  //   moveList: {
-  //     w: 800,
-  //     h: 2400,
-  //     x: 60,
-  //     y: 120,
-  //   },
-  //   viewBox: {
-  //     w: 4180 + (visibleMoveList ? 860 : 0) + (visiblePieceBox ? 860 : 0), // 2220 + 800 + 800 + 120 * 2 + 60 * 2 + ? + ?
-  //     h: 2640, // 2400 + 120 * 2
-  //   },
-  // };
-
-  $: viewBoxWidth = 4180 + (visibleMoveList ? 860 : 0) + (visiblePieceBox ? 860 : 0);
+  $: viewBoxWidth = 3920 + (visibleMoveList ? 860 : 0) + (visiblePieceBox ? 860 : 0);
   const viewBoxHeight = 2640;
 
   const handleToggleTurn = () => {
@@ -284,7 +240,7 @@
       {pickedPiece}
     />
     <Board
-      x={980}
+      x={850}
       y={120}
       blackBoard={position.blackBoard}
       whiteBoard={position.whiteBoard}
@@ -292,19 +248,20 @@
       onRightClick={handleRightClick}
       {pickedPiece}
     />
+    <!-- y = 120 + 10 + 260*5 + 5*5 - 10 -->
     <PieceStand
-      x={3320}
-      y={1160}
+      x={3190}
+      y={1445}
       isBlack={true}
       hands={position.blackHands}
       onAreaClick={handlePieceClick}
       onPieceClick={handlePieceClick}
       {pickedPiece}
     />
-    <TurnIndicator x={3320} y={120} isBlackTurn={position.isBlackTurn} onClick={handleToggleTurn} />
+    <TurnIndicator x={3190} y={120} isBlackTurn={position.isBlackTurn} onClick={handleToggleTurn} />
     {#if visiblePieceBox}
       <PieceBox
-        x={4180}
+        x={3920}
         y={840}
         pieces={position.pieceBox}
         onAreaClick={handlePieceClick}
@@ -312,9 +269,10 @@
         {pickedPiece}
       />
     {/if}
+    <!-- 850 + (2220 / 2) - (900 / 2) -->
     {#if promoteChoice !== PieceType.VACANCY}
       <PromoteIndicator
-        x={1640}
+        x={1310}
         y={1010}
         pieceType={promoteChoice}
         onSelect={handleSelectPoromote}
