ee4bd737d09774844e34c9acd61cbc27ef627132
Saiki Toshiki <toshiki.jcytp@gmail.com>
2025-02-01 22:47:15 +0900

    adjust layout for mobile

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -4,8 +4,10 @@
   import '$lib/styles/default.scss';
   import { account, sessionToken } from '$lib/stores/session';
   import { onMount } from 'svelte';
+  import { navigating } from '$app/stores';
   import { page } from '$app/stores';
   import { goto } from '$app/navigation';
+  import { Menu } from 'lucide-svelte';
 
   const isCurrentPath = (path: string) => {
     const currentPath = $page.url.pathname;
@@ -41,20 +43,44 @@
   const logout = () => {
     sessionToken.set(null);
   };
+
+  // ----------------------------------------
+  // メニューの展開
+  let isMenuOpen = false;
+  const toggleMenu = () => {
+    isMenuOpen = !isMenuOpen;
+  };
+
+  $: if ($navigating) {
+    // ページ遷移が発生したらメニューを閉じる
+    isMenuOpen = false;
+  }
 </script>
 
 <div class="app">
   <header>
+    <button class="menu-button" onclick={toggleMenu}>
+      <Menu size={27} color="var(--primary-color)" />
+    </button>
     <h1><a href="/" class="logo">棋譜UP</a></h1>
   </header>
-  <nav>
+  {#if isMenuOpen}
+    <div
+      class="overlay"
+      role="button"
+      tabindex="0"
+      onclick={toggleMenu}
+      onkeydown={(e) => e.key === 'Escape' && toggleMenu()}
+    ></div>
+  {/if}
+  <nav class:open={isMenuOpen}>
     <ul>
       {#if isLoggedIn}
         <li><a href="/home/" class:active={isCurrentPath('/home/')}>ホーム</a></li>
         <li><a href="/kifu/new/" class:active={isCurrentPath('/kifu/new/')}>新規作成</a></li>
         <li><a href="/kifu/search/" class:active={isCurrentPath('/kifu/search/')}>棋譜検索</a></li>
         <li><a href="/settings/" class:active={isCurrentPath('/settings/')}>設定</a></li>
-        <li class="newgroup"><button on:click={logout}>ログアウト</button></li>
+        <li class="newgroup"><button onclick={logout}>ログアウト</button></li>
       {:else}
         <li><a href="/" class:active={isCurrentPath('/')}>ログイン</a></li>
         <li><a href="/kifu/search/" class:active={isCurrentPath('/kifu/search/')}>棋譜検索</a></li>
@@ -70,16 +96,35 @@
 </div>
 
 <style lang="scss">
+  @import '../lib/styles/mixins.scss';
+
   .app {
     display: flex;
     flex-direction: column;
     min-height: 100vh;
 
     header {
+      @include sp {
+        padding: 0.5rem 0 0 3.5rem;
+      }
+      @include pc {
+        padding: 0.5rem 0 0 11rem;
+      }
       background-color: var(--background-color);
       border-bottom: 0.3rem var(--primary-color) solid;
       padding: 0.5rem 0 0 11rem;
       z-index: var(--z-index-header);
+
+      .menu-button {
+        @include sp {
+          position: absolute;
+          top: 1rem;
+          left: 1rem;
+        }
+        @include pc {
+          display: none;
+        }
+      }
 
       .logo {
         color: var(--primary-color);
@@ -90,10 +135,38 @@
       }
     }
 
+    .overlay {
+      @include sp {
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        width: 100%;
+        height: 100%;
+        z-index: var(--z-index-nav);
+        background: rgba(0, 0, 0, 0.2);
+      }
+      @include pc {
+        display: none;
+      }
+    }
+
     nav {
+      @include sp {
+        left: -100%; // 初期状態は画面外
+        transition: left 0.3s ease;
+        box-shadow: 2px 0 8px rgba(0, 0, 0, 0.2);
+
+        &.open {
+          left: 0; // メニュー表示
+        }
+      }
+      @include pc {
+        left: 0;
+      }
       position: fixed;
       top: 0;
-      left: 0;
       bottom: 0;
       z-index: var(--z-index-nav);
       background-color: var(--background-color);
@@ -138,8 +211,13 @@
     }
 
     main {
+      @include sp {
+        padding: 1.5rem 0 2rem 0;
+      }
+      @include pc {
+        padding: 1.5rem 0 2rem 10rem;
+      }
       flex: 1;
-      padding: 1.5rem 0 2rem 10rem;
       max-width: 72rem;
       width: 100%;
       box-sizing: border-box;
